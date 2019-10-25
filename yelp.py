567a4aa1049ddbc6d6e7e51fb778d0af3e2fb754
Luke Norrell <lukenorrell@utexas.edu>
2019-10-24 23:37:39 -0500

    updating for yasira

--- yelp.py
+++ yelp.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from yelp.client import Client
 
 print("Hello")
 print("__name__ value: ", __name__)
@@ -13,10 +14,17 @@ def main():
     response = requests.get("https://munch-server.herokuapp.com/contributors")
     jprint(response.json())
     print(response.status_code)
-
     result = json.loads(response)
-
     print(result)
+
+    MY_API_KEY = "LkR2_IcEI64ibyBqKbF5QSX1fIIHbut01A-F2lOAPcic8XlA-gMnn0R2s79gy3LMLUYiEPWOqUpqqkVORbxF94HljLFUocMvLYXTNZMuQcxGuf58lxjZIQg8C3WyXXYx"  # Replace this with your real API key
+    client = Client(MY_API_KEY)
+
+    #client = getClient("yelp")
+
+    business_response = client.business.get_by_id('four seasons austin')
+
+    print(business_response)
 
 if __name__ == '__main__':
     main()
